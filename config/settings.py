69069daab2aa68f74be7bc48b7c31a37535bf684
davetes <tesfa4706@gmail.com>
2025-10-26 19:47:49 +0300

    add roots

--- config/settings.py
+++ config/settings.py
@@ -129,9 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
 
 # Media (user-uploaded files)
 MEDIA_URL = '/media/'
